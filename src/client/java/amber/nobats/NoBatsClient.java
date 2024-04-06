ea24f8e5fc2e3dc0f587d5677cb44365c21cb997
Amber <13422754+AmberRoseZA@users.noreply.github.com>
2024-04-06 14:59:40 +0200

    Add files via upload

---
+++ src/client/java/amber/nobats/NoBatsClient.java
@@ -0,0 +1,10 @@
+package amber.nobats;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoBatsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
